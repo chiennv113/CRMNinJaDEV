5bc5e3c86fbcef95a536fcaea26955eedf32c43f
chien <chiennvph06113@fpt.edu.vn>
2019-10-08 09:29:24 +0700

    abc
    Commit

---
+++ app/src/main/java/com/example/crm/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.example.crm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int TIME_OUT = 2000;
+    private Handler handler = null;
+    private Runnable runnable = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        handler.postDelayed(runnable, TIME_OUT);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacks(runnable);
+        handler = null;
+        runnable = null;
+    }
+}
