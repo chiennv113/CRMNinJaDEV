295379985946bcfff478c861acefaacfaa952182
chien <chiennvph06113@fpt.edu.vn>
2019-10-09 16:36:36 +0700

    Update Backend Search
    abc

--- app/src/main/java/com/example/crm/Model/ModelLogin/Data.java
+++ app/src/main/java/com/example/crm/Model/ModelLogin/Data.java
@@ -1,5 +1,5 @@
 
-package com.example.crm.ModelLogin;
+package com.example.crm.Model.ModelLogin;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
--- app/src/main/java/com/example/crm/Retrofit/ServiceRetrofit.java
+++ app/src/main/java/com/example/crm/Retrofit/ServiceRetrofit.java
@@ -1,12 +1,29 @@
 package com.example.crm.Retrofit;
 
-import com.example.crm.ModelLogin.Login;
+import android.content.Context;
+import android.util.Log;
+
+import com.example.crm.Model.ModelLogin.Login;
+import com.example.crm.Model.ModelSearchCu.Search;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ServiceRetrofit {
 //    https://crm.phanmemninja.com/api/userapi
     @POST("api/userapi")
     Call<Login> createUser(@Body Login login);
+
+
+
+
+    @POST("api/userapi")
+    Call<Search> search(@Query("infocheck") String info,@Query("option") String option, @Header("Cookie") String cookie, @Header("Content-Type") String content);
 }
