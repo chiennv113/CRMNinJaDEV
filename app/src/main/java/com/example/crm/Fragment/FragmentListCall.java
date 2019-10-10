50c807cbae48b3e63b0e8b694671be8e9f849149
chien <chiennvph06113@fpt.edu.vn>
2019-10-10 11:19:59 +0700

    Comit
    Comit

--- app/src/main/java/com/example/crm/Fragment/FragmentListCall.java
+++ app/src/main/java/com/example/crm/Fragment/FragmentListCall.java
@@ -5,10 +5,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.crm.Model.ModelSearchCu.Search;
 import com.example.crm.R;
@@ -21,18 +25,40 @@ import retrofit2.Response;
 
 public class FragmentListCall extends Fragment {
     private ServiceRetrofit service;
+    private static final String KEY_COOKIE = "FragmentListCall.KEY_COOKIE";
+    private EditText mEdtInfoSearch;
+    private ImageButton mBtnSearch;
+    private RecyclerView mRecycleviewRemind;
+    private TextView mTvTest;
+
+    public static Fragment newInstance(String cookie) {
+        Fragment fragment = new FragmentListCall();
+        Bundle args = new Bundle();
+        args.putString(KEY_COOKIE, cookie);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_listcall, container, false);
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        initView(view);
         service = ApiClient.getClient().create(ServiceRetrofit.class);
+//        String strtext = getArguments().getString("edttext");
+//        Log.e("abcde", "onCreateView: "+strtext);
 
-        search("0979090897",
-                "search_customer",
-                "crm_session=eyJpdiI6IkMyZkJXXC9pV00wN1JWZjA5SERRQVR3PT0iLCJ2YWx1ZSI6Iks1SFlCNUp1K1FFTmhWcmx5UmZrN0tvazl6QkVZS0FcL1ZEZVJ2dExqWEFRYTNKT0J5Wm9LU0dMYU9SalBXVWo1IiwibWFjIjoiYmY3YTFhYzQwNGFiOWNmZTIyMjcwZmJlM2Q0ZjJjNzQ2NzlhYTQyZTFmMmI4ZjU1YzdkN2Y4YWJmYzllYmFlNCJ9; expires=Thu, 10-Oct-2019 07:39:50 GMT; Max-Age=86400; path=/; httponly",
-                "application/x-www-form-urlencoded");
-
+        search("0979090897","search_customer","crm_ninja=eyJpdiI6IkxNXC9Sblg2bHIyNTBRNlRGUHE2Z1dnPT0iLCJ2YWx1ZSI6ImJaYmxGUWxmQjFXZjBrWkZyaXFxZzlMSGdJMjBRQ0lzbzVLSG5IbHMrcXZ5aVhyd2l1WUdkUjdhOE8xZTE1RFkiLCJtYWMiOiIxNWMwZDk1ZTA5NWQ0MThjZDBlNjZhNTA0ODg4NjM2YWVlMGRmNjQ4NjMwYjg5ZWM0MzEyMDhjNDhlMzFiMWZhIn0%3D; expires=Fri, 11-Oct-2019 04:11:41 GMT; Max-Age=86400; path=/; httponly","application/x-www-form-urlencoded");
         return view;
     }
+
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        if (getArguments() != null) {
+//            String cookie = getArguments().getString(KEY_COOKIE);
+//            Log.e("hehehe", "" + cookie);
+//        }
+//    }
 
     public void search(String info, String option, String cookie, String content) {
         final Search abc = new Search("0979090897", "search_customer");
@@ -49,6 +75,12 @@ public class FragmentListCall extends Fragment {
 
             }
         });
+    }
 
+    private void initView(View view) {
+        mEdtInfoSearch = view.findViewById(R.id.edtInfoSearch);
+        mBtnSearch = view.findViewById(R.id.btnSearch);
+        mRecycleviewRemind = view.findViewById(R.id.recycleview_remind);
+        mTvTest = view.findViewById(R.id.tvTest);
     }
 }
