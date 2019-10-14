271648f4fa4dccec1dbc934f5b25957fc61a1e3a
chien <chiennvph06113@fpt.edu.vn>
2019-10-14 13:35:21 +0700

    sd

--- app/src/main/java/com/example/crm/Fragment/FragmentListCall.java
+++ app/src/main/java/com/example/crm/Fragment/FragmentListCall.java
@@ -43,14 +43,12 @@ public class FragmentListCall extends Fragment {
     private static final String KEY_COOKIE = "FragmentListCall.KEY_COOKIE";
     private EditText mEdtInfoSearch;
     private ImageView imgViewSearch;
-    // private ImageView mBtnSearch;
     private RecyclerView mRecycleviewRemind;
 
 
     private static final String KEY_COO_KIE = "FragmentListCall.KEY_COOKIE";
-    private EditText mEdtInfoSearch;
     private ImageView mBtnSearch;
-    private RecyclerView mRecycleviewRemind;
+
 
 
     public static Fragment newInstance(String cookie) {
@@ -65,43 +63,10 @@ public class FragmentListCall extends Fragment {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
         initView(view);
-        service = ApiClient.getClient().create(ServiceRetrofit.class);
-
-
-        //  imgViewSearch=view.findViewById(R.id.imgViewSearch);
-        //  mEdtInfoSearch = view.findViewById(R.id.edtInfoSearch);
-//        imgViewSearch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(getActivity(), "Tim kiemkiem", Toast.LENGTH_LONG ).show();
-//            }
-//        });
-//        mEdtInfoSearch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(getActivity(), "Please long press the key", Toast.LENGTH_LONG ).show();
-//            }
-//        });
-
-//        String strtext = getArguments().getString("edttext");
-//        Log.e("abcde", "onCreateView: "+strtext);
-
-        search("0979090897", "search_customer", "crm_ninja=eyJpdiI6IkxNXC9Sblg2bHIyNTBRNlRGUHE2Z1dnPT0iLCJ2YWx1ZSI6ImJaYmxGUWxmQjFXZjBrWkZyaXFxZzlMSGdJMjBRQ0lzbzVLSG5IbHMrcXZ5aVhyd2l1WUdkUjdhOE8xZTE1RFkiLCJtYWMiOiIxNWMwZDk1ZTA5NWQ0MThjZDBlNjZhNTA0ODg4NjM2YWVlMGRmNjQ4NjMwYjg5ZWM0MzEyMDhjNDhlMzFiMWZhIn0%3D; expires=Fri, 11-Oct-2019 04:11:41 GMT; Max-Age=86400; path=/; httponly", "application/x-www-form-urlencoded");
-
-
         return view;
 
     }
 
-
-    //    @Override
-//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//        if (getArguments() != null) {
-//            String cookie = getArguments().getString(KEY_COOKIE);
-//            Log.e("hehehe", "" + cookie);
-//        }
-//    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -169,4 +134,4 @@ public class FragmentListCall extends Fragment {
     }
 
 
-}
+
